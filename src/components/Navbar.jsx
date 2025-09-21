a31432a7fb4d799696bab3e26f02a667ffedec6a
adamadiga90 <atheadigaman83@gmail.com>
2025-09-21 19:17:31 +0300

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
-    <div className="bg-[#45474B] p-5 text-[#F4CE14] font-bold text-[30px]">
+    <div className="bg-[#45474B] p-5 text-[#F4CE14] font-bold text-[30px] flex gap-40">
       Sudoku
+      <button
+        onClick={() => setIsVisible(true)}
+        className="bg-[#F4CE14] text-[#45474B] p-2"
+      >
+        {isVisible ? (
+          <span>Welcome Zaynab</span>
+        ) : (
+          <span>is your name Zaynab</span>
+        )}
+      </button>
     </div>
   );
 };
